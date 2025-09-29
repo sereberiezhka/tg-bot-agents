14aaf2ccfcafc342c05f6e472a7ee9052d3e95b4
Stepan <56102302+sereberiezhka@users.noreply.github.com>
2025-09-29 10:26:52 +0000

    begin1

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+BOT_TOKEN = "8171756915:AAFiZFAEBx2yJxtiSkMlDjFbEPKTMXjZjSk"
+
+# ID канала для архива фото (должен начинаться с -100)
+PHOTO_ARCHIVE_CHANNEL_ID = -1003129591507 # Замени на свой ID
+
+# Название файла с ключами от Google API
+GOOGLE_CREDS_JSON = "miralko-bot-dfc195fd0788.json"
+
+# Название твоей Google Таблицы
+GOOGLE_SHEET_NAME = "Отчеты по агентам"
+
